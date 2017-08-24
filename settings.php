62660e75088a028639a4a30e6a76b0bad7f29bcb
Nelson Parreira <nelson@ahmic.be>
2017-08-24 10:25:01 +0200

    DB Settings defined.  Please enter the commit message for your changes. Lines starting

---
+++ settings.php
@@ -0,0 +1,5 @@
+<?php
+defined('DB_HOST') or define('DB_HOST', 'localhost');
+defined('DB_USER') or define('DB_USER', 'root');
+defined('DB_PASS') or define('DB_PASS', 'root');
+defined('DB_NAME') or define('DB_NAME', 'newdatabase');
